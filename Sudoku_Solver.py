21f2d83754e934340c2ff4f3f7b769361ed02953
Balakrishna Prabhu B N <64630414+Bluejee@users.noreply.github.com>
2023-02-07 14:39:06 +0530

    Added the License notice to the head

--- Sudoku_Solver.py
+++ Sudoku_Solver.py
@@ -1,4 +1,23 @@
 '''
+Sudoku_Solver - A basic program to input a sudoku and find all possible solutions
+
+Copyright (C) 20223  Balakrishna Prabhu B. N. <balakrishnaprabhu1999@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+    
+-----------------------------------------------------------------------------------------------------------------------
+
 This program will take in a 9*9 matrix of sudoku as an input and then return the solved sudoku as output.
 For that we will use functions.
 1) To check weather a number is possible at a position.
